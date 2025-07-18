c7c2aca4b48e75deb5362d6a3fa4eaadb002caa6
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-07-18 21:46:29 +0100

    dynamo db

---
+++ src/main/java/com/api/sisi_yemi/config/DynamoDbConfig.java
@@ -0,0 +1,17 @@
+package com.api.sisi_yemi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+    }
+}
