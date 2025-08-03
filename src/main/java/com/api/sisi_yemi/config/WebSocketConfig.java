9c5556658d9a63aea522ee560a6c9e9aa525979e
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-08-03 22:41:45 +0100

    update websocket v2

--- src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
+++ src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
@@ -28,7 +28,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-messages")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
