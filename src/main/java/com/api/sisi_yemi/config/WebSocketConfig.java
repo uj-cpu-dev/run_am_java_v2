f95dc136e0fff468959cb52a25f50138bf9965b8
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-08-03 21:17:21 +0100

    update websocket

--- src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
+++ src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
@@ -22,6 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
