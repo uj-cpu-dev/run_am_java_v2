5dfa7f8e40ce2d0ff95b24ddc30f96b8d4dcaef3
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-08-04 18:18:35 +0100

    update websocket v11

--- src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
+++ src/main/java/com/api/sisi_yemi/config/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-messages")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("https://runam.africa", "https://www.runam.africa")
                 .withSockJS()
                 .setSessionCookieNeeded(false);
     }
